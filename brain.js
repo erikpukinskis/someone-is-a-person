b6334ee474fb7159594ec1979501a6c27fe56b03
Erik Pukinskis <erik@snowedin.net>
2017-06-30 13:11:27 -0700

    brain refactor

--- brain.js
+++ brain.js
@@ -1,11 +1,16 @@
 
+
+
+
+
+
+
 var frame = 900
+
 
 
 // Boot
 
-var t = 0
-setInterval(tick, frame)
 var clock = document.querySelector(".clock")
 
 function cellNode(height, slide) {
@@ -34,18 +39,59 @@ var state = {
   right: "stance",
   left: "stance",
 }
+var bod
+var mover
+var right
+var left
+var t = 0
+
+
+
+var legs
+
+function lift(which) {
+  legs[which].classList.remove(state[which])
+  legs[which].classList.add("raised")
+  state[which] = "raised"
+  if (which == "right") {
+    bod.style["transform-origin"] = "2em 4em"
+    bod.style.transform = "rotate(20deg) translateY(0em)"
+  } else {
+    bod.style["transform-origin"] = "4em 4em"
+    bod.style.transform = "rotate(-20deg) translateY(0em)"
+  }
+}
+
+function power(which) {
+  legs[which].classList.remove("raised")
+  legs[which].classList.add("power")
+  state[which] = "power"
+
+  var other = which == "right" ? "left" : "right"
+  legs[other].classList.remove("power")
+  legs[other].classList.add("stance")
+  state[other] = "stance"
+
+  legs[which].style.transform = "scaleY(1.0)"
+  bod.style.transform = "rotate(0deg) translateY(-0.3em)"
+}
+
+
+
+// step 0: Solid box below each leg, tap right
+// step 1: leg collapses
+// step 2: tap and hold right to give it strength, tap left
+// step 3: [has air]
+// step 4: tap and hold left to give it strength, tap right
+// step 4: [has air]
+// ... step 2 again
 
 
 function tick() {
   if (t>50) {
     return
   }
-  var bod = document.querySelector(".bod")
-  var mover = document.querySelector(".mover")
-  if (!mover) { console.log("no mover"); return }
-    
-  var right = cellNode(1,2)
-  var left = cellNode(1,0)
+
   y += 0.25
 
   mover.style.transform = "translateY("+y+"em)"
@@ -57,56 +103,10 @@ function tick() {
     var background = document.querySelector(".background")
     background.style.transform = "translateY("+y+"em)"
   } 
-
-  var legs = {
-    left: left,
-    right: right,
-  }
-    
-  function lift(which) {
-    legs[which].classList.remove(state[which])
-    legs[which].classList.add("raised")
-    console.log("adding raised to ", which)
-    state[which] = "raised"
-    if (which == "right") {
-      bod.style["transform-origin"] = "2em 4em"
-      bod.style.transform = "rotate(20deg) translateY(0em)"
-    } else {
-      bod.style["transform-origin"] = "4em 4em"
-      bod.style.transform = "rotate(-20deg) translateY(0em)"
-    }
-  }
-  
-  function power(which) {
-    if (state[which] != "raised") {
-      console.log("existing state on "+which+" is "+state[which])
-      console.log("existing classes are "+legs[which].classList)
-    }
-    
-    legs[which].classList.remove("raised")
-    legs[which].classList.add("power")
-    state[which] = "power"
-
-    var other = which == "right" ? "left" : "right"
-    legs[other].classList.remove("power")
-    legs[other].classList.add("stance")
-    state[other] = "stance"
-
-    legs[which].style.transform = "scaleY(1.0)"
-    bod.style.transform = "rotate(0deg) translateY(-0.3em)"
-  }
   
   var mod = (t-1)%4+1
 
   switch(mod) {
-
-  // step 0: Solid box below each leg, tap right
-  // step 1: leg collapses
-  // step 2: tap and hold right to give it strength, tap left
-  // step 3: [has air]
-  // step 4: tap and hold left to give it strength, tap right
-  // step 4: [has air]
-  // ... step 2 again
       
   case 1:
     lift("right")
@@ -127,3 +127,15 @@ function tick() {
 
 
 
+setTimeout(function() {
+  bod = document.querySelector(".bod")
+  mover = document.querySelector(".mover")
+  if (!mover) { console.log("no mover"); return }
+
+  legs = {
+    left: cellNode(1,0),
+    right: cellNode(1,2),
+  }
+  
+  setInterval(tick, frame)
+})
