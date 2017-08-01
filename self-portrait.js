780d0a104b49d74b07801bbbd4d6af1b0e901a89
Erik Pukinskis <erik@snowedin.net>
2017-08-01 15:38:52 -0700

    portraits

---
+++ self-portrait.js
@@ -0,0 +1,21 @@
+function (paintSwatch) {
+  paintSwatch({"minX":73,"minY":19,"maxX":167,"maxY":162}, "rgb(255, 228, 217)")
+  paintSwatch({"minX":131,"minY":98,"maxX":148,"maxY":115}, "rgb(204, 164, 150)")
+  paintSwatch({"minX":56,"minY":103,"maxX":61,"maxY":108}, "rgb(225, 171, 143)")
+  paintSwatch({"minX":51,"minY":100,"maxX":68,"maxY":117}, "rgb(184, 165, 152)")
+  paintSwatch({"minX":33,"minY":66,"maxX":84,"maxY":85}, "rgb(246, 205, 185)")
+  paintSwatch({"minX":101,"minY":8,"maxX":187,"maxY":31}, "rgb(138, 127, 126)")
+  paintSwatch({"minX":-4,"minY":274,"maxX":233,"maxY":407}, "rgb(70, 70, 72)")
+  paintSwatch({"minX":82,"minY":234,"maxX":167,"maxY":317}, "rgb(221, 171, 137)")
+  paintSwatch({"minX":83,"minY":235,"maxX":168,"maxY":252}, "rgb(150, 106, 76)")
+  paintSwatch({"minX":52,"minY":157,"maxX":173,"maxY":235}, "rgb(199, 160, 131)")
+  paintSwatch({"minX":79,"minY":167,"maxX":114,"maxY":184}, "rgb(246, 234, 210)")
+  paintSwatch({"minX":85,"minY":173,"maxX":121,"maxY":184}, "rgb(148, 104, 99)")
+  paintSwatch({"minX":43,"minY":153,"maxX":79,"maxY":183}, "rgb(189, 134, 89)")
+  paintSwatch({"minX":96,"minY":150,"maxX":144,"maxY":167}, "rgb(221, 181, 157)")
+  paintSwatch({"minX":161,"minY":57,"maxX":190,"maxY":226}, "rgb(144, 110, 89)")
+  paintSwatch({"minX":53,"minY":238,"maxX":82,"maxY":315}, "rgb(188, 58, 62)")
+  paintSwatch({"minX":61,"minY":268,"maxX":97,"maxY":303}, "rgb(190, 117, 107)")
+  paintSwatch({"minX":108,"minY":317,"maxX":125,"maxY":402}, "rgb(232, 242, 247)")
+  // begin
+}
