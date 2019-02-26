dd13ac7030502d42f41d8d160836be3a3e317b85
Erik Pukinskis <erik@snowedin.net>
2019-02-25 21:23:27 -0800

    voxel spec

---
+++ voxel.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+there's a dot for each item
+
+and it's on the grid somewhere
+
+it can be dragged to others dots
+
+it has an element underneath that physicses around hanging from the dot
+
+if you tap the dots next to a dot, the item below gets bigger
+
+some things just appear four dots
+
+others appear one
+
+as events happen you float up
+
+there are branches and stuff, so you can sense how high you are
+
+
