12d60ca6e160bc1c1c619dde492107f5230d3f4e
Erik Pukinskis <erik@snowedin.net>
2019-02-25 21:46:23 -0800

    Move everything relevant to the walk animation into brain, everything with a nice interface. Magically slow motion works fine now.

---
+++ boot-brain.js
@@ -0,0 +1,77 @@
+var library = require("module-library")(require)
+
+library.using(
+  [library.ref(), "web-site", "web-element", "./brain", "browser-bridge", "bridge-module"],
+  function(lib, WebSite, element, brain, BrowserBridge, bridgeModule) {
+
+    var site = new WebSite()
+    var bridge = new BrowserBridge()
+    site.start(5550)
+
+    var FRAME_LENGTH_IN_MS = brain.FRAME_LENGTH_IN_MS
+
+    bridge.domReady([
+      bridgeModule(lib, "./brain", bridge)],
+      function(startBrain) {
+        startBrain() })
+
+    // Brain
+
+    var brainCells = [
+      [null,"stance",null,null],
+      ["stance",null,"stance",null],
+    ]
+
+    var highs = brainCells[0]
+    var lows = brainCells[1]
+
+    var page = element(".page",
+      element.style({
+        "position": "absolute",
+        "left": "5em",
+        "top": "5em",
+      }),
+    )
+
+    brain.addTo(bridge)
+
+    var background = element(".background")
+    var body = element(".bod.gravity")
+
+    for(var height=0; height<brainCells.length; height++) {
+
+      for(var slide=0; slide<brainCells[0].length; slide++) {
+        var thought = brainCells[height][slide]
+        var neuron = brain.cell(height, slide, thought)
+
+        if (thought == "stance") {
+          // if (height == 1) {
+          //   neuron.appendStyles({visibility: "hidden"})
+          // }
+          body.addChild(neuron)
+        } else {
+          background.addChild(neuron)
+        }
+      }
+    }
+
+    page.addChildren(background, element(".mover.gravity", body))
+
+    var clock = element(".clock", "0", element.style({"font-family": "sans-serif"}))
+
+    page.addChild(clock)
+
+    site.addRoute(
+      "get",
+      "/",
+      bridge.requestHandler(page)
+    )
+
+  }
+)
+
+
+
+
+
+
--- brain.js
+++ brain.js
@@ -1,138 +1,247 @@
 
+var library = require("module-library")(require)
+
+module.exports = library.export(
+  "someone-is-a-person/brain",[
+  "web-element"],
+  function(element) {
+
+    var FRAME_LENGTH_IN_MS = 2000
+
+
+
+    function backlog() {}
+
+    // I am smoking so much and it does very little
+
+    // I did get tobacco (for free!) but it's still very true that I think this pot has run its course for me.
+
+    // It's worth noting that this is a week
+
+    // I should ask Marie in the future for a week not a day. It's traumatic for her
+
+    // But in lieu of cannabis, I have decided to solve programming problems:
+
+
+    backlog("someone-is-a-person",[
+      "walk cycle will stretch to other nearby squares if you click them",
+      "Paint your head",
+      "Paint your body (choose one color)",
+      "Pants can have multiple segments (max 3), but they are only posed once per frame",
+      "Choose your legs",
+      "Skinny jeans",
+      "Baggy blues",
+      "Naked",
+      ])
 
 
 
 
 
-var FRAME_LENGTH_IN_MS = 250
 
-// Boot
+    // Templates
 
-var clock = document.querySelector(".clock")
+    var cell = element.template(
+      ".cell.gravity",
+      element.style({
+        "display": "block",
+        "position": "absolute",
+        "width": "2em",
+        "height": "2em",
+        "background": "#eee",
+      }),
+      function(height, slide, thought) {
 
-function cellNode(height, slide) {
-  return document.querySelector(".height-"+height+".slide-"+slide)
-}
+        this.appendStyles({
+          "left": slide*2+"em",
+          "top": height*2+"em",
+          "transform-origin": slide*2+"em "+height*2+"em",
+        })
 
-var y = 0
+        this.addSelector(".height-"+height+".slide-"+slide)
 
-
-var key = {
-  up: 38,
-  down: 40,
-  right: 39,
-  left: 37,
-}
-
-document.querySelector("html").addEventListener('keydown', function(event){
-  if (event.which == key.left) {
-    console.log("left!")
-  }
-}, false);
+        if (thought == "stance") {
+          this.addSelector(".stance")
+        }
+      }
+    )
 
 
-
-var state = {
-  right: "stance",
-  left: "stance",
-}
-var bod
-var mover
-var right
-var left
-var t = 0
+    var rowStyle = element.style(".row", {
+    })
 
 
 
-var legs
 
-function lift(which) {
-  legs[which].classList.remove(state[which])
-  legs[which].classList.add("raised")
-  state[which] = "raised"
-  if (which == "right") {
-    bod.style["transform-origin"] = "2em 4em"
-    bod.style.transform = "rotate(20deg) translateY(0em)"
-  } else {
-    bod.style["transform-origin"] = "4em 4em"
-    bod.style.transform = "rotate(-20deg) translateY(0em)"
-  }
-}
+    var stance = element.style(".stance", {
+      "background": "cyan",
+    })
 
-function power(which) {
-  legs[which].classList.remove("raised")
-  legs[which].classList.add("power")
-  state[which] = "power"
+    var raised = element.style(".cell.raised", {
+      "background": "transparent",
+      "box-sizing": "border-box",
+      "border": "2px solid cyan",
+      "height": "1em",
+      "vertical-align": "top",
+    })
 
-  var other = which == "right" ? "left" : "right"
-  legs[other].classList.remove("power")
-  legs[other].classList.add("stance")
-  state[other] = "stance"
+    var powerStyle = element.style(".cell.power", {
+      "background": "black",
+      "transition": "height "+FRAME_LENGTH_IN_MS+"ms"
+    })
 
-  legs[which].style.transform = "scaleY(1.0)"
-  bod.style.transform = "rotate(0deg) translateY(-0.3em)"
-}
+    var gravityStyle = element.style(".gravity", {
+      "transition": "transform "+FRAME_LENGTH_IN_MS+"ms",
+      "transition-timing-function": "ease-in", // http://stackoverflow.com/a/15427614/778946
+    })
+
+    var bodyStyle = element.style(".bod", {
+      "transform-origin": "2em 4em",
+    })
 
 
 
-// step 0: Solid box below each leg, tap right
-// step 1: leg collapses
-// step 2: tap and hold right to give it strength, tap left
-// step 3: [has air]
-// step 4: tap and hold left to give it strength, tap right
-// step 4: [has air]
-// ... step 2 again
 
 
-function tick() {
-  if (t>50) {
-    return
-  }
 
-  y += 0.25
 
-  mover.style.transform = "translateY("+y+"em)"
 
-  t++
-  clock.innerHTML = t.toString()
 
-  if ((t-1) % 8 == 1) {
-    var background = document.querySelector(".background")
-    background.style.transform = "translateY("+y+"em)"
-  } 
-  
-  var mod = (t-1)%4+1
 
-  switch(mod) {
+    // Original Brain Boot Stuff
+
+    var clock
+    var state
+    var bod
+    var mover
+    var right
+    var left
+    var t
+    var legs
+    var y
+
+    function brain(node) {
+      state = {
+        right: "stance",
+        left: "stance",
+      }
+      t = 0
+      y = 0
+
+      clock = document.querySelector(".clock")
+
+      document.querySelector("html").addEventListener('keydown', function(event){
+        if (event.key == "ArrowLeft") {
+          console.log("left!")
+        }
+      }, false);
+
+      setTimeout(function() {
+        bod = document.querySelector(".bod")
+        mover = document.querySelector(".mover")
+        if (!mover) { console.log("no mover"); return }
+
+        legs = {
+          left: cellNode(1,0),
+          right: cellNode(1,2),
+        }
+        
+        setInterval(tick, FRAME_LENGTH_IN_MS)
+      })
+
+    }
+
+    function cellNode(height, slide) {
+      return document.querySelector(".height-"+height+".slide-"+slide)
+    }
+
+    function lift(which) {
+      legs[which].classList.remove(state[which])
+      legs[which].classList.add("raised")
+      state[which] = "raised"
+      if (which == "right") {
+        bod.style["transform-origin"] = "2em 4em"
+        bod.style.transform = "rotate(20deg) translateY(0em)"
+      } else {
+        bod.style["transform-origin"] = "4em 4em"
+        bod.style.transform = "rotate(-20deg) translateY(0em)"
+      }
+    }
+
+    function power(which) {
+      legs[which].classList.remove("raised")
+      legs[which].classList.add("power")
+      state[which] = "power"
+
+      var other = which == "right" ? "left" : "right"
+      legs[other].classList.remove("power")
+      legs[other].classList.add("stance")
+      state[other] = "stance"
+
+      legs[which].style.transform = "scaleY(1.0)"
+      bod.style.transform = "rotate(0deg) translateY(-0.3em)"
+    }
+
+
+
+    // step 0: Solid box below each leg, tap right
+    // step 1: leg collapses
+    // step 2: tap and hold right to give it strength, tap left
+    // step 3: [has air]
+    // step 4: tap and hold left to give it strength, tap right
+    // step 4: [has air]
+    // ... step 2 again
+
+
+    function tick() {
+      if (t>50) {
+        return
+      }
+
+      y += 0.25
+
+      mover.style.transform = "translateY("+y+"em)"
+
+      t++
+      clock.innerHTML = t.toString()
+
+      if ((t-1) % 8 == 1) {
+        var background = document.querySelector(".background")
+        background.style.transform = "translateY("+y+"em)"
+      } 
       
-  case 1:
-    lift("right")
-  break;
-  case 2:
-    power("right")
-  break;
-  case 3:
-    lift("left")
-  break;
-  case 4:
-    power("left")
-  break;
-  }
-  
+      var mod = (t-1)%4+1
 
-}
+      switch(mod) {
+          
+      case 1:
+        lift("right")
+      break;
+      case 2:
+        power("right")
+      break;
+      case 3:
+        lift("left")
+      break;
+      case 4:
+        power("left")
+      break;
+      }
+    }
 
+    brain.FRAME_LENGTH_IN_MS = FRAME_LENGTH_IN_MS
 
+    brain.addTo = function(bridge){
+      if (bridge.remember("someone-is-a-person/brain")) {
+        return }
 
-setTimeout(function() {
-  bod = document.querySelector(".bod")
-  mover = document.querySelector(".mover")
-  if (!mover) { console.log("no mover"); return }
+      bridge.addToHead(
+        element.stylesheet(rowStyle, cell, gravityStyle, stance, raised, powerStyle, bodyStyle))
 
-  legs = {
-    left: cellNode(1,0),
-    right: cellNode(1,2),
-  }
-  
-  setInterval(tick, FRAME_LENGTH_IN_MS)
-})
+      bridge.see("someone-is-a-person/brain", true)
+    }
+
+    brain.cell = cell
+    
+    return brain
+  })
